8ab32844ae7bccea1db7ab9c64dfaefeec073dc8
Deepattic <deepattic@example.mail>
2024-03-19 17:45:55 +0530

    A simple health_check

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use actix_learning::run;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let addr = TcpListener::bind("127.0.0.1:8080").expect("Failed To Bind localhost:8080");
+    run(addr)?.await
 }
---
+++ tests/health_check.rs
@@ -0,0 +1,27 @@
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn health_check_works() {
+    // Arrange
+    let address = spawn_app();
+    // We need to bring in `reqwest`
+    // to perform HTTP requests against our application.
+    let client = reqwest::Client::new();
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", &address))
+        .send()
+        .await
+        .expect("Failed to execute request.");
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to Bind Address");
+    let port = listener.local_addr().unwrap().port();
+    let server = actix_learning::run(listener).expect("Faild to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
